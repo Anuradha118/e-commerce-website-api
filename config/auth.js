299bea08383350c261b9071eb89eb56d577d1404
Anuradha118 <34187745+Anuradha118@users.noreply.github.com>
2018-02-17 20:35:18 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,18 +1,18 @@
 module.exports = {
 	'facebookAuth' : {
-		"FB_CLIENT_ID":193898741343872,
-        "FB_CLIENT_SECRET":"e63aebadb6559f26eb096e6b29231a60",
-        "FB_CALLBACK_URL":"http://localhost:3000/users/login/facebook/callback"
+		"FB_CLIENT_ID":'your fb client id',
+        "FB_CLIENT_SECRET":"your fb cliet secret",
+        "FB_CALLBACK_URL":"your fb call url"
 	},
 
 	'googleAuth' : {
-		"GOOGLE_CLIENT_ID":"8922537660-5lnik636d903h2tncr0o4do99ub32vhl.apps.googleusercontent.com",
-        "GOOGLE_CLIENT_SECRET":"Ml8PQyFMXK7GGG6eb25zKw4E",
-        "GOOGLE_CALLBACK_URL":"http://localhost:3000/users/login/google/callback"
+		"GOOGLE_CLIENT_ID":"your google client id",
+        "GOOGLE_CLIENT_SECRET":"your google client secret",
+        "GOOGLE_CALLBACK_URL":"your google callback url"
     },
     
     'sendgridAuth':{
-        "SENDGRID_USER":"anuradha93",
-        "SENDGRID_SECRET":"snehalata@14"
+        "SENDGRID_USER":"your sendgrid user",
+        "SENDGRID_SECRET":"your sendgrid password"
     }
 }
